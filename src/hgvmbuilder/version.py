1e16bc04d399ca999ac189cdde074358b2be9aa7
Adam Novak <anovak@soe.ucsc.edu>
2017-07-13 13:27:09 -0700

    Allow newer TSV

--- src/hgvmbuilder/version.py
+++ src/hgvmbuilder/version.py
@@ -2,7 +2,7 @@ version = '0.1.0'
 
 required_versions = {
     'toil': '>=3.6.0',
-    'tsv': '==1.1',
+    'tsv': '>=1.1',
     'biopython': '==1.68',
     'pyvcf': '==0.6.8',
     'pysam': '==0.10.0',
